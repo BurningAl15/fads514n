d7a6b32d089b2a25980ce410961899cb5b588f37
Aldhair Vera <aldhairvera15@gmail.com>
2022-01-04 17:50:52 -0500

    fix: show list data

--- src/App.js
+++ src/App.js
@@ -14,7 +14,13 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {
+            this.state.list.map((value, index) => {
+              return <li key={index}>
+                {value}
+              </li>
+            })
+          }
         </ul>
       </div>
     );
